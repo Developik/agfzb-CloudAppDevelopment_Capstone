5d9955a7303e70e59cf14b8ba6f46fa7e050d920
andrii <provozin99@gmail.com>
2021-06-11 04:45:47 +0000

    finished registration system/login

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,10 +14,12 @@ urlpatterns = [
     # path for contact us view
     path('contact/', views.contact, name='contact'),
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
 
     # path for logout
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
